2b5838435751ee5b50d5b8438ca12160046c2706
k9want <zndwlrnjr26@daum.net>
2023-09-26 23:25:45 +0900

    :technologist: Add hello-template.html & HelloController - MVC와 템플릿 엔진

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+
+    //http://localhost:8080/hello-mvc?name=spring
+    // @RequestParam의 required default값은 true다
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
