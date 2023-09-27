7560b72f3d9e0722a702ced9c4c78a612880966d
k9want <zndwlrnjr26@daum.net>
2023-09-27 17:35:53 +0900

    :technologist: Add 회원 서비스 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     * */
+    public Long join(Member member) {
+        // 종복 회원 x
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(mem -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        validDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(mem -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
